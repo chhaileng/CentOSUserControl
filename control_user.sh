f44e17cd1c93a7c96825dedb5bd7d262933518d4
Chhaileng Peng <Chhaileng@Chhailengs-MacBook-Pro.local>
2017-10-12 12:27:22 +0700

    update and delete user

--- control_user.sh
+++ control_user.sh
@@ -4,102 +4,148 @@
 
 # Functions Area
 check_root_user() {
-	if [ "$EUID" -ne 0 ]
-		then echo "[-] Permission denied! Are you root?"
-		exit
-	fi
+if [ "$EUID" -ne 0 ]
+then echo "[-] Permission denied! Are you root?"
+exit
+fi
 }
-#cut -d: -f1 /etc/passwd
+
 menu() {
-	echo "#--------------------MENU---------------------"
-	echo "#     1. Create User from File                "
-	echo "#     2. Delete User                          "
-	echo "#     3. Update User                          "
-	echo "#     4. Create Group                         "
-	echo "#     5. Add User to Group                    "
-	echo "#     0. Exit                                 "
-	echo -n "#     Choose options: "
+echo "#--------------------MENU---------------------"
+echo "#     1. Create Users from File               "
+echo "#     2. Delete User                          "
+echo "#     3. Update User                          "
+echo "#     4. Create Group                         "
+echo "#     5. Add User to Group                    "
+echo "#     6. Show all users in system             "
+echo "#     0. Exit                                 "
+echo -n "#     Choose options: "
 }
+
+menu_update_user() {
+echo "#--------------------MENU---------------------"
+echo "#     1. Update Information                   "
+echo "#     2. Change Password                      "
+echo "#     0. Back                                 "
+echo -n "#     [Update User] options: "
+}
+
 pause() {
-	echo -n $1
-	read
+echo -n $1
+read
 }
 
 add_user() {
-	egrep "^$1" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "[-] User $1 exists!"
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $2)
-		useradd -m -p $pass $1
-		[ $? -eq 0 ] && echo "[+] User has been added to system!" || echo "[-] Failed to add a user!"
-	fi
+egrep "^$1" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+echo "[-] User $1 exists!"
+else
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $2)
+useradd -m -p $pass $1
+[ $? -eq 0 ] && echo "[+] User has been added to system!" || echo "[-] Failed to add a user!"
+fi
 }
 
+
 controller() {
-	users=()
-	while IFS='' read -r user || [[ -n "$user" ]]; do
-		if ! [ -z $user ]; then
-   			users+=("$user")
-		fi
-	done < "$1"
-	echo "[+] Found ${#users[@]} users"
-	while true
-	do
-		menu
-		read choose
-		case $choose in 
-			"1")	count=1
-				for i in ${users[@]}
-				do
-					IFS=':' read -ra record <<< "$i"
-					echo "User record : $count"
-					echo "Username: ${record[0]}"
-					echo "Password: ${record[1]}"
-					read -p "[*] Create this user? [y/*/exit]: " yn
-					if [ "$yn" == "y" -o "$yn" == "Y" ]; then
-						add_user ${record[0]} ${record[1]}
-					elif [ "$yn" == "exit" ]; then
-						break
-					else
-						echo "[-] Create user canceled!"
-					fi
-					pause "Press enter to continue..."
-					clear
-				done
-			;;
-			"2")	echo "2"
-				pause "Press enter to continue..."
-			;;
-			"0") exit
-			;;
-			*) echo "Command Not Found"
-			;;		
-		esac
-		clear
-	done
+users=()
+while IFS='' read -r user || [[ -n "$user" ]]; do
+if ! [ -z $user ]; then
+users+=("$user")
+fi
+done < "$1"
+echo "[+] Found ${#users[@]} users"
+while true
+do
+menu
+read choose
+case $choose in
+"1")    count=1
+for i in ${users[@]}
+do
+IFS=':' read -ra record <<< "$i"
+echo "User record : $count"
+echo "Username: ${record[0]}"
+echo "Password: ${record[1]}"
+read -p "[*] Create this user? [y/*/exit]: " yn
+if [ "$yn" == "y" -o "$yn" == "Y" ]; then
+add_user ${record[0]} ${record[1]}
+elif [ "$yn" == "exit" ]; then
+break
+else
+echo "[-] Create user canceled!"
+fi
+pause "Press enter to continue..."
+count=`expr $count + 1`
+clear
+done
+;;
+"2")    read -p "[*] Enter username: " usr
+userdel $usr > /dev/null 2>&1
+del_status=$?
+if [ $del_status -eq 0 ]; then
+echo "[+] User $usr deleted successfully!"
+else
+echo "[-] User $usr is not exist!"
+fi
+pause "Press enter to continue..."
+;;
+"3")    read -p "[*] Enter username: " usr
+id $usr > /dev/null 2>&1
+usr_status=$?
+if [ $usr_status -eq 0 ]; then
+while true
+do
+menu_update_user
+read c
+case $c in
+"1")    chfn $usr
+pause "Press enter to continue..."
+;;
+"2")    passwd $usr
+pause "Press enter to continue..."
+;;
+"0")    break
+;;
+esac
+clear
+done
+else
+echo "[-] User $usr is not exist!"
+pause "Press enter to continue..."
+fi
+;;
+"6")    clear
+cut -d: -f1 /etc/passwd
+pause "Press enter to continue..."
+;;
+"0") exit
+;;
+*) echo "Command Not Found"
+;;
+esac
+clear
+done
 }
 
 main() {
-	read -p "[*] Enter users file name (blank if users.txt): " file_name
-	#file_name="users.txt"
+read -p "[*] Enter users file name (users.txt): " file_name
+#file_name="users.txt"
 
-	if [ -z $file_name ]; then
-		file_name="users.txt"
-	fi
+if [ -z $file_name ]; then
+file_name="users.txt"
+fi
 
-	if ! [ -s $file_name ]; then
-		echo "[-] File $file_name is not found!"
-	else
-		echo "[+] Using file $file_name..."
-		echo "[+] Reading user from file..."
+if ! [ -s $file_name ]; then
+echo "[-] File $file_name is not found!"
+else
+echo "[+] Using file $file_name..."
+echo "[+] Reading user from file..."
 
-		controller $file_name
-	fi
+controller $file_name
+fi
 }
 
 # Main Area
 check_root_user
-
 main
-
