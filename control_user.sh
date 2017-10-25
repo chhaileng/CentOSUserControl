c306e38608a6eb7c5bd86c3d1683f62f8669c2d5
Chhaileng Peng <Chhaileng@Chhailengs-MacBook-Pro.local>
2017-10-25 15:34:43 +0700

    Final

--- control_user.sh
+++ control_user.sh
@@ -16,8 +16,11 @@ echo "#     1. Create Users from File               "
 echo "#     2. Delete User                          "
 echo "#     3. Update User                          "
 echo "#     4. Create Group                         "
-echo "#     5. Add User to Group                    "
-echo "#     6. Show all users in system             "
+echo "#     5. Delete group                         "
+echo "#     6. Add User to Group                    "
+echo "#     7. Show all users in system             "
+echo "#     8. Show all groups in system            "
+echo "#     9. Show account information             "
 echo "#     0. Exit                                 "
 echo -n "#     Choose options: "
 }
@@ -26,6 +29,8 @@ menu_update_user() {
 echo "#--------------------MENU---------------------"
 echo "#     1. Update Information                   "
 echo "#     2. Change Password                      "
+echo "#     3. Add Description to User              "
+echo "#     4. Change Username (Login Name)         "
 echo "#     0. Back                                 "
 echo -n "#     [Update User] options: "
 }
@@ -66,22 +71,13 @@ do
 IFS=':' read -ra record <<< "$i"
 echo "User record : $count"
 echo "Username: ${record[0]}"
-echo "Password: ${record[1]}"
-read -p "[*] Create this user? [y/*/exit]: " yn
-if [ "$yn" == "y" -o "$yn" == "Y" ]; then
 add_user ${record[0]} ${record[1]}
-elif [ "$yn" == "exit" ]; then
-break
-else
-echo "[-] Create user canceled!"
-fi
-pause "Press enter to continue..."
 count=`expr $count + 1`
-clear
 done
+pause "Press enter to continue..."
 ;;
 "2")    read -p "[*] Enter username: " usr
-userdel $usr > /dev/null 2>&1
+userdel -r $usr > /dev/null 2>&1
 del_status=$?
 if [ $del_status -eq 0 ]; then
 echo "[+] User $usr deleted successfully!"
@@ -105,6 +101,16 @@ pause "Press enter to continue..."
 "2")    passwd $usr
 pause "Press enter to continue..."
 ;;
+"3")    read -p "[*] Description (no space): " cmt
+usermod -c $cmt $usr
+cat /etc/passwd | grep $usr
+pause "Press enter to continue..."
+;;
+"4")    read -p "[*] Enter new username: " n_usr
+usermod -l $n_usr $usr
+echo "[+] Username update successfully!"
+pause "Press enter to continue..."
+;;
 "0")    break
 ;;
 esac
@@ -115,9 +121,41 @@ echo "[-] User $usr is not exist!"
 pause "Press enter to continue..."
 fi
 ;;
-"6")    clear
+"4")    #clear
+read -p "[*] Enter group name: " gro
+groupadd $gro
+pause "Press enter to continue..."
+;;
+"5")    #clear
+read -p "[*] Enter group name: " gro
+groupdel $gro
+pause "Press enter to continue..."
+;;
+"6")    #clear
+read -p "[*] Enter group name: " gro
+read -p "[*] Enter user name: " usr
+usermod -G $gro $usr
+pause "Press enter to continue..."
+;;
+"7")    clear
 cut -d: -f1 /etc/passwd
 pause "Press enter to continue..."
+;;
+"8")    clear
+cat /etc/group
+pause "Press enter to continue..."
+;;
+"9")    #clear
+read -p "[*] Enter username: " usr
+id $usr > /dev/null 2>&1
+usr_status=$?
+if [ $usr_status -eq 0 ]; then
+chage -l $usr
+pause "Press enter to continue..."
+else
+echo "[-] User not found!"
+pause "Press enter to continue..."
+fi
 ;;
 "0") exit
 ;;
@@ -149,3 +187,4 @@ fi
 # Main Area
 check_root_user
 main
+
